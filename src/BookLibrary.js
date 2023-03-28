dff2253d2adaa779fb9542400ea2884f62995c0a
Pakize Bozkurt <pakiozihak@gmail.com>
2023-03-28 22:27:27 +0100

    Updated BookLibrary.js

--- src/BookLibrary.js
+++ src/BookLibrary.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function BookLibrary() {
+function BookLibrary({ books }) {
   return (
     <div>
-      
+      <h1>My Library</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            <Link to={`/books/${book.id}`}>{book.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default BookLibrary;
