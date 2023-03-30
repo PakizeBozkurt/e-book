8be644244077292d63f32e7e0615bb79e52982eb
Pakize Bozkurt <pakiozihak@gmail.com>
2023-03-30 23:24:43 +0100

    Updated app.js

--- server/app.js
+++ server/app.js
@@ -16,11 +16,9 @@ const pool = new Pool({
 
 app.use(cors());
 
-
 // Configure middleware
 
 app.use(bodyParser.json());
-
 
 app.get("/books", (req, res) => {
   // Retrieve all books from the database
@@ -32,70 +30,64 @@ app.get("/books", (req, res) => {
     }
   });
 });
-// bookRouter
-//   .route("/")
-//   .get((req, res) => {})
-//   .post((req, res) => {
-//     // Create a new book and save it to the database
-//     const book = req.body;
-//     const values = [book.title, book.author, book.content];
-//     pool.query(
-//       "INSERT INTO books (title, author, content) VALUES ($1, $2, $3) RETURNING *",
-//       values,
-//       (err, result) => {
-//         if (err) {
-//           res.send(err);
-//         } else {
-//           res.json(result.rows[0]);
-//         }
-//       }
-//     );
-//   });
 
-// bookRouter
-//   .route("/:id")
-//   .get((req, res) => {
-//     // Retrieve a specific book from the database
-//     const id = req.params.id;
-//     pool.query("SELECT * FROM books WHERE id = $1", [id], (err, result) => {
-//       if (err) {
-//         res.send(err);
-//       } else {
-//         res.json(result.rows[0]);
-//       }
-//     });
-//   })
-//   .put((req, res) => {
-//     // Update a specific book in the database
-//     const id = req.params.id;
-//     const book = req.body;
-//     const values = [book.title, book.author, book.content, id];
-//     pool.query(
-//       "UPDATE books SET title = $1, author = $2, content = $3 WHERE id = $4 RETURNING *",
-//       values,
-//       (err, result) => {
-//         if (err) {
-//           res.send(err);
-//         } else {
-//           res.json(result.rows[0]);
-//         }
-//       }
-//     );
-//   })
-//   .delete((req, res) => {
-//     // Delete a specific book from the database
-//     const id = req.params.id;
-//     pool.query("DELETE FROM books WHERE id = $1", [id], (err, result) => {
-//       if (err) {
-//         res.send(err);
-//       } else {
-//         res.sendStatus(204);
-//       }
-//     });
-//   });
+app.post((req, res) => {
+  // Create a new book and save it to the database
+  const book = req.body;
+  const values = [book.title, book.author, book.content];
+  pool.query(
+    "INSERT INTO books (title, author, content) VALUES ($1, $2, $3) RETURNING *",
+    values,
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(result.rows[0]);
+      }
+    }
+  );
+});
 
-// // Register the bookRouter and start the server
-// app.use("/books", bookRouter);
+app.get((req, res) => {
+  // Retrieve a specific book from the database
+  const id = req.params.id;
+  pool.query("SELECT * FROM books WHERE id = $1", [id], (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result.rows[0]);
+    }
+  });
+});
+
+app.put((req, res) => {
+  // Update a specific book in the database
+  const id = req.params.id;
+  const book = req.body;
+  const values = [book.title, book.author, book.content, id];
+  pool.query(
+    "UPDATE books SET title = $1, author = $2, content = $3 WHERE id = $4 RETURNING *",
+    values,
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(result.rows[0]);
+      }
+    }
+  );
+});
+app.delete((req, res) => {
+  // Delete a specific book from the database
+  const id = req.params.id;
+  pool.query("DELETE FROM books WHERE id = $1", [id], (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.sendStatus(204);
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
