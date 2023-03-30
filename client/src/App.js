f227d373756b11be3ba5b7f4f4ab2a88fd4ea894
Pakize Bozkurt <pakiozihak@gmail.com>
2023-03-30 23:32:44 +0100

    Formating

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Route, Routes } from "react-router-dom";
 import BookLibrary from "./components/BookLibrary";
 import BookReader from "./components/BookReader";
 import "./App.css";
 
 function App() {
-  
-
   return (
     <div className="App">
       <Routes>
